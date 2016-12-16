f2a25e861dbba7d2082369b684ee377cf5f5d710
il-marcio <il-marcio@acasa.terrore>
2016-12-16 22:23:04 +0100

    big red button

--- public/player/app.js
+++ public/player/app.js
@@ -36,20 +36,35 @@ socket.on('message', function (sessionId) {
 });
 
 socket.on('player list cleaned', function () {
-  $('#roll-button').prop("disabled", false);
-  $('#roll-button').addClass("btn-big-red");
-  $('#roll-button').removeClass("btn-big-disabled");  
+  enableButton();
+  cleanRollResult();
 });
 
 $('#roll-button').click(function() {
-  $('#roll-button').prop("disabled", true);
-  $('#roll-button').removeClass("btn-big-red");
-  $('#roll-button').addClass("btn-big-disabled");
+  disableButton();
   socket.emit('player rolling', currentPlayerInfo); 
 });
 
 var updateRollResult = function(playerInfo) { 
-  $('#roll-result').html("<strong>" + playerInfo.initiativeRoll + "</strong> (" + playerInfo.diceResult + " + " + playerInfo.initiativeModifier + ")"); 
+  $('#roll-result').html(playerInfo.initiativeRoll); 
+  $('#roll-result-partial').html(playerInfo.diceResult + " + " + playerInfo.initiativeModifier); 
+};
+
+var cleanRollResult = function(playerInfo) { 
+  $('#roll-result').html(""); 
+  $('#roll-result-partial').html(""); 
+};
+
+var disableButton = function () {
+  $('#roll-button').prop("disabled", true);
+  $('#roll-button').removeClass("btn-big-red");
+  $('#roll-button').addClass("btn-big-disabled");
+};
+
+var enableButton = function () {
+  $('#roll-button').prop("disabled", false);
+  $('#roll-button').addClass("btn-big-red");
+  $('#roll-button').removeClass("btn-big-disabled");
 };
 
 socket.on('my roll result', updateRollResult);
@@ -64,13 +79,9 @@ var getPlayerInfo = function () {
   }).done(function(playerInfo) {
     if(playerInfo && playerInfo.initiativeRoll) {
       updateRollResult(playerInfo);
-      $('#roll-button').prop("disabled", true);
-      $('#roll-button').removeClass("btn-big-red");
-      $('#roll-button').addClass("btn-big-disabled");
+      disableButton();
     } else {
-      $('#roll-button').prop("disabled", false);
-      $('#roll-button').addClass("btn-big-red");
-      $('#roll-button').removeClass("btn-big-disabled");
+      enableButton();
     }
   });
 };
